f47c52c9badd7fff7acd821ece869a15ecedb4c2
moviesivan <35140824+moviesivan@users.noreply.github.com>
2018-01-05 16:57:10 +0300

    Add files via upload

---
+++ lab45/src/main/java/org/lwjglb/engine/graph/Texture.java
@@ -0,0 +1,54 @@
+package org.lwjglb.engine.graph;
+
+import de.matthiasmann.twl.utils.PNGDecoder;
+import de.matthiasmann.twl.utils.PNGDecoder.Format;
+import java.nio.ByteBuffer;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.glGenerateMipmap;
+
+public class Texture {
+
+    private final int id;
+
+    public Texture(String fileName) throws Exception {
+        this(loadTexture(fileName));
+    }
+
+    public Texture(int id) {
+        this.id = id;
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private static int loadTexture(String fileName) throws Exception {
+        PNGDecoder decoder = new PNGDecoder(Texture.class.getResourceAsStream(fileName));
+
+        ByteBuffer buf = ByteBuffer.allocateDirect(
+                4 * decoder.getWidth() * decoder.getHeight());
+        decoder.decode(buf, decoder.getWidth() * 4, Format.RGBA);
+        buf.flip();
+
+        int textureId = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0,
+                GL_RGBA, GL_UNSIGNED_BYTE, buf);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        return textureId;
+    }
+
+    public void cleanup() {
+        glDeleteTextures(id);
+    }
+}
